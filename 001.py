5b042690119b0baad67bc3dfaa3e9b5397a8e2be
vapvin <tpehdj12@daum.net>
2019-09-21 23:29:40 +0900

    first Upload

---
+++ 001.py
@@ -0,0 +1,14 @@
+my_tuple2 = 'python', 'javascript', 'jQuery'
+print(my_tuple2)
+
+print('''첫 번째
+두번째
+세번째''')
+
+
+fruit_str = 'apple banana lemon'
+fruits = fruit_str.split()
+print(fruits[0])
+
+thisis = 'python'
+print(thisis[:3])
